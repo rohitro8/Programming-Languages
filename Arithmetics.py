c5aeb52f01ee1c786cc6fae01391189734b6f9d7
rohitro8 <p.rohit.r08@gmail.com>
2026-05-27 11:32:51 +0530

    File added

---
+++ Arithmetics.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number "))
+y = int(input("Enter 2nd number "))
+
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} x {y} = {x * y}")
+print(f"{x} / {y} = {x / y}")
